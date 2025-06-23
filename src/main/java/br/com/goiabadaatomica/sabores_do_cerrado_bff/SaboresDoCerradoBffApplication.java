f683b177b684ad49f25b7a04b7c83a62127049bd
Victor Hugo Marega <victorhugomarega@hotmail.com>
2025-06-23 20:06:25 -0300

    Commit inicial

---
+++ src/main/java/br/com/goiabadaatomica/sabores_do_cerrado_bff/SaboresDoCerradoBffApplication.java
@@ -0,0 +1,13 @@
+package br.com.goiabadaatomica.sabores_do_cerrado_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaboresDoCerradoBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaboresDoCerradoBffApplication.class, args);
+	}
+
+}
